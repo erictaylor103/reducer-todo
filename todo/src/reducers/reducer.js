f7fb5720be9b82c2d521677a47f9f28361858279
Eric Taylor <erictaylor@Erics-MacBook-Pro.local>
2020-05-11 21:13:36 -0400

    created reducer folder - added the reducer file - set initialState - created the reducer function

---
+++ todo/src/reducers/reducer.js
@@ -0,0 +1,13 @@
+import React, { useReducer } from 'react';
+
+const initialState = {
+    item: "Learn about reducers",
+    completed: false,
+    id: 3892987589
+}
+
+const reducer = (state, action) => {
+    return state;
+}
+
+export default reducer;
